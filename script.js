1548417a3d13a034ebfb39f8b23cc5439b426900
ellalah <126103809+ellalah@users.noreply.github.com>
2023-04-10 19:02:23 +0100

    New edits after feedback

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-let tabs = document.querySelectorAll (".tabs h3");
-let tabContents = document.querySelectorAll (".tab-content div");
+function sendEmail (){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "ehalstead92@googlemail.com",
+        Password : "J0ssBut13r21!",
+        To : 'ehalstead92@googlemail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Submission",
+        Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Message: " + document.getElementById("message").value
 
-tabs.forEach((tab, index) )
+    }).then(
+    message => alert("Message Sent Successfully")
+    );
+}
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
