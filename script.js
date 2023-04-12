a32b99bf4b41794524ca2d69495bd86e9c2e7ed6
ellalah <126103809+ellalah@users.noreply.github.com>
2023-04-12 18:00:35 +0100

    last min changes

--- script.js
+++ script.js
@@ -15,18 +15,16 @@ function sendEmail (){
     );
 }
 
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
+
+$(document).ready(function() {
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
     } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
     }
-  }
+  });
+});
